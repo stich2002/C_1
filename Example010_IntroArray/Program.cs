4100199a981fd1ff8801efee1e5c67a2ea48df33
Elena <elena15112002@gmail.com>
2022-09-03 19:20:51 +0500

    save11

---
+++ Example010_IntroArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int [] array = {1, 54, 65, 87, 99, 51, 20, 0};
+
+int n = array.Length;
+int find = 99;
+int index = 0;
+
+while (index<n)
+{
+    if(array[index]==find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
