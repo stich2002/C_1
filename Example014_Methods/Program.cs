fca6fa8a9407bb4872638455411c1bc9a32a752c
Elena <elena15112002@gmail.com>
2022-09-09 21:43:13 +0500

    save17

---
+++ Example014_Methods/Program.cs
@@ -0,0 +1,34 @@
+﻿// Упорядочить массив
+
+int [] arr = {1, 5, 8, 6, 7, 5, 1, 2, 9};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array) 
+{
+    for (int i = 0; i < array.Length - 1 ; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
